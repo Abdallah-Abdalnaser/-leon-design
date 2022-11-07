d2a579f0a7db8b5e07eec55977893a47186b8259
Abdallah abdalnaser <111485030+Abdallah-Abdalnaser@users.noreply.github.com>
2022-11-07 22:18:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let icon = document.querySelector("header .contener .links .icon");
+let span2 = document.querySelector("header .contener .links .icon span:nth-child(2)");
+let ul = document.querySelector("header .contener .links ul");
+document.addEventListener("click",function(e){
+  console.log(e.target.classList.contains("show"));
+  if (e.target.classList.contains("show")) {
+    ul.style.display= "block";
+    span2.style.width="100%";
+    icon.classList.toggle("show");
+  } else {
+    ul.style.display= "none";
+    span2.style.width="60%";
+    icon.classList.toggle("show");
+  }
+});
