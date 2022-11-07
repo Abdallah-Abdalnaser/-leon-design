e8a017734b1035e615ca4f95a533dcd90eeaf47d
Abdallah abdalnaser <111485030+Abdallah-Abdalnaser@users.noreply.github.com>
2022-11-07 22:43:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let icon = document.querySelector("header .contener .links .icon");
 let span2 = document.querySelector("header .contener .links .icon span:nth-child(2)");
 let ul = document.querySelector("header .contener .links ul");
-document.addEventListener("click",function(e){
-  console.log(e.target.classList.contains("show"));
+let loading = document.querySelector(".loading");
+document.addEventListener("click",(e)=>{
   if (e.target.classList.contains("show")) {
     ul.style.display= "block";
     span2.style.width="100%";
@@ -13,3 +13,4 @@ document.addEventListener("click",function(e){
     icon.classList.toggle("show");
   }
 });
+window.addEventListener("load",()=>{loading.style.display="none"});
